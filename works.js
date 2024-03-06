ea8a514bf9f042af94752b3c4b2c9caae689b584
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-03-06 10:43:54 +0530

    formate document

--- works.js
+++ works.js
@@ -1,56 +1,56 @@
 
 // javascrip file code
 
-function lodepage(){
+function lodepage() {
     var t1 = gsap.timeline();
 
-    t1.to(".background",{
-        top:"-100%",
-        delay:0.5,
-        duration:0.5,
+    t1.to(".background", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 0.5,
         ease: "expo.Out"
     })
-    t1.from("nav",{
-        opacity:0,
-        delay:0.1
-    },'h')
-    t1.from(".bottom",{
-        opacity:0,
-        delay:0.1
-    },'h')
-    t1.to(".loder video",{
-        top:"-100%",
-        delay:0.5,
-        duration:1,
+    t1.from("nav", {
+        opacity: 0,
+        delay: 0.1
+    }, 'h')
+    t1.from(".bottom", {
+        opacity: 0,
+        delay: 0.1
+    }, 'h')
+    t1.to(".loder video", {
+        top: "-100%",
+        delay: 0.5,
+        duration: 1,
         ease: "power4.out"
-    },'a')
-    t1.to(".loder",{
-        opacity:0,
-        delay:0.5,
-        duration:1,
+    }, 'a')
+    t1.to(".loder", {
+        opacity: 0,
+        delay: 0.5,
+        duration: 1,
         ease: "power4.out"
-    },'a')
+    }, 'a')
 
-    t1.to(".menu a,.menu i",{
-        color:"black",
-        delay:0.5,
-        duration:1,
+    t1.to(".menu a,.menu i", {
+        color: "black",
+        delay: 0.5,
+        duration: 1,
         ease: "power4.out"
-    },'a')
-    t1.to(".bottom h5,.bottom button",{
-        color:"black",
-        delay:0.5,
-        duration:1,
+    }, 'a')
+    t1.to(".bottom h5,.bottom button", {
+        color: "black",
+        delay: 0.5,
+        duration: 1,
         ease: "power4.out"
-    },'a')
-    t1.to(".logo svg",{
-        fill:"black",
-        delay:0.5,
-        duration:1,
+    }, 'a')
+    t1.to(".logo svg", {
+        fill: "black",
+        delay: 0.5,
+        duration: 1,
         ease: "power4.out"
-    },'a')
-    t1.to(".loder",{
-        display:"none",
+    }, 'a')
+    t1.to(".loder", {
+        display: "none",
         ease: "power4.out"
     })
 }
