987590e030cb7fc0c92f358cd67658a5a9971550
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-03-06 10:42:05 +0530

    fixed all buges

--- works.js
+++ works.js
@@ -1,45 +1,65 @@
-// locomotive
-
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.main'),
-//     smooth: true
-// });
 
 // javascrip file code
 
-var t1 = gsap.timeline();
+function lodepage(){
+    var t1 = gsap.timeline();
 
-t1.to(".background",{
-    top:"-100%",
-    delay:0.5,
-    duration:0.5,
-    ease: "expo.Out"
-})
-t1.from("nav",{
-    opacity:0,
-    delay:0.1
-})
-t1.to(".loder video",{
-    top:"-100%",
-    delay:0.5,
-    duration:1,
-    ease: "power4.out"
-},'a')
-t1.to(".loder",{
-    opacity:0,
-    delay:0.5,
-    duration:1,
-    ease: "power4.out"
-},'a')
-t1.to(".menu a,.menu i",{
-    color:"black",
-    delay:0.5,
-    duration:1,
-    ease: "power4.out"
-},'a')
-t1.to(".logo svg",{
-    fill:"black",
-    delay:0.5,
-    duration:1,
-    ease: "power4.out"
-},'a')
+    t1.to(".background",{
+        top:"-100%",
+        delay:0.5,
+        duration:0.5,
+        ease: "expo.Out"
+    })
+    t1.from("nav",{
+        opacity:0,
+        delay:0.1
+    },'h')
+    t1.from(".bottom",{
+        opacity:0,
+        delay:0.1
+    },'h')
+    t1.to(".loder video",{
+        top:"-100%",
+        delay:0.5,
+        duration:1,
+        ease: "power4.out"
+    },'a')
+    t1.to(".loder",{
+        opacity:0,
+        delay:0.5,
+        duration:1,
+        ease: "power4.out"
+    },'a')
+
+    t1.to(".menu a,.menu i",{
+        color:"black",
+        delay:0.5,
+        duration:1,
+        ease: "power4.out"
+    },'a')
+    t1.to(".bottom h5,.bottom button",{
+        color:"black",
+        delay:0.5,
+        duration:1,
+        ease: "power4.out"
+    },'a')
+    t1.to(".logo svg",{
+        fill:"black",
+        delay:0.5,
+        duration:1,
+        ease: "power4.out"
+    },'a')
+    t1.to(".loder",{
+        display:"none",
+        ease: "power4.out"
+    })
+}
+lodepage()
+
+// locomotive
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
