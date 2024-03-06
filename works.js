fca8c0ee840b1b8f601832af8338f6293c1bc288
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-03-06 16:37:54 +0530

    page 3 with images

--- works.js
+++ works.js
@@ -56,20 +56,106 @@ function lodepage() {
 }
 lodepage()
 
-// locomotive
+// locomotive + scrolltrigger
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+function locoscr() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+
+        // follwoing line is not required to work pinning on touch screen
+
+        /* pinType: document.querySelector(".mainl").style.transform
+          ? "transform"
+          : "fixed"*/
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+locoscr()
 
 // image effect in page 2
 
-var ele = document.querySelectorAll(".elem")
-var bg = document.querySelector(".page2")
-ele.forEach(function(el){
-    el.addEventListener("mouseenter",()=>{
+function page2img(){
+    var ele = document.querySelectorAll(".elem")
+    var bg = document.querySelector(".page2")
+    ele.forEach(function (el) {
+    el.addEventListener("mouseenter", () => {
         var images = el.getAttribute("data-img");
         bg.style.backgroundImage = `url(${images})`;
     })
 })
+}
+page2img()
+
+// page2 nav change
+
+function navchange_color(){
+    var t2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: "nav",
+          scroller: ".main",
+        //   markers:true,
+          start: "top -100%",
+          scrub: 1
+        }
+      })
+    
+    t2.to(".logo svg",{
+        fill:"#ffffff"
+    },'time')
+    
+    t2.to(".menu a,.menu i",{
+        color:"#ffffff"
+    },'time')
+    
+    
+    var t3 = gsap.timeline({
+        scrollTrigger: {
+          trigger: "nav",
+          scroller: ".main",
+        //   markers:true,
+          start: "top -185%",
+          scrub: 1
+        }
+      })
+    
+    t3.to(".logo svg",{
+        fill:"#000000"
+    },'time')
+    
+    t3.to(".menu a,.menu i",{
+        color:"#000000"
+    },'time')
+    
+}
+navchange_color()
